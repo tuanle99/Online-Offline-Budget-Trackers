a2937a25366fe33559d4dc2bb9b9bed7bf77196c
tuanhoangle <tuanleprofessional@gmail.com>
2021-04-24 14:35:13 -0400

    mongo atlas

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Online-Offline-Budget-Trackers
+
+Budget Trackers is a project that allow the user to add and subtract fund offline and save, then when the user go back online, those data is then added to the database.
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
+//|| "mongodb://localhost/budget"
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
