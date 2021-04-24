a97d8ca756c9a5fd9d1d31a4c7aa72ee1e290204
tuanhoangle <tuanleprofessional@gmail.com>
2021-04-24 14:45:01 -0400

    env

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,8 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-//|| "mongodb://localhost/budget"
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
